4ba203b587f9c05146fc7aaff0848438badcf955
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-02-13 12:15:11 -0300

    1 day

---
+++ 100-days-swift.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+//STRINGS
+var greeting = "Hello, playground"
+
+print(greeting.uppercased())
+print(greeting.hasPrefix("Hel"))
+print(greeting.hasPrefix("Hal"))
+
+//INTS
+let num = 130
+let numAux = 13_00 //1300
+print(num.isMultiple(of: 3))
