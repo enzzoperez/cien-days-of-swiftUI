e939d77d20ec7d173ca46354e53ffba4439a693c
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-02-20 15:50:25 -0300

    6 days and checkpoint swift

--- 100-days-swift.playground/Contents.swift
+++ 100-days-swift.playground/Contents.swift
@@ -1,61 +1,35 @@
-let aux1 = "YAnderson"
-let aux2 = "Bikers"
+//var lyric = "Haters gonna"
+//
+//for i in lyric {
+//    print("my: ", i)
+//}
+//
+//
+//print("Tu", 100 % 1000)
 
-//with strings  sign >< and = compare the alphabetical order
-if(aux1 < aux2){
-    print("first \(aux1) and \(aux2)")
-} else {
-    print("first \(aux2) and \(aux1)")
+
+//continues
+//let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+//
+//for filename in filenames {
+//    if filename.hasSuffix(".jpg") == false {
+//        break
+//    }
+//
+//    print("Found picture: \(filename)")
+//}
+
+
+//Checkpont: fizzbuzz problem
+
+for n in 1...100 {
+    if n % 3 == 0 && n % 5 == 0 {
+        print("fizz buzz")
+    } else if(n % 3 == 0) {
+        print("Fizz")
+    } else if(n % 5 == 0) {
+        print("buzz")
+    } else {
+        print("number: ", n)
+    }
 }
-
-
-//isEmpty is more performant that =="" or .count == 0
-let c = "a"
-let d = "aux"
-
-if(d.isEmpty ){
-    print("VACIO")
-} else {
-    print("CONTIENE LETRAS")
-}
-
-//comparition with enums
-enum TransportOption {
-    case airplane, bus, car, helicopter, biclycle, scooter
-}
-
-enum Movies {
-    case batman, superman, marvel, dc, spiderman
-}
-
-let transport = TransportOption.helicopter
-
-if(transport == .bus || transport == .car){ //only dots because Swift knows transport must be some kind of TransportOption
-    print("Trafic!")
-} else if(transport == .airplane || transport == .helicopter){
-    print("Flying!")
-} else {
-    print("Walking")
-}
-
-
-//switch fallthrough
-let day = 5
-print("My true love gave to me…")
-
-switch day {
-case 5:
-    print("5 golden rings")
-    fallthrough
-case 4:
-    print("4 calling birds")
-    fallthrough
-case 3:
-    print("3 French hens")
-case 2:
-    print("2 turtle doves")
-default:
-    print("A partridge in a pear tree")
-}
-
-
