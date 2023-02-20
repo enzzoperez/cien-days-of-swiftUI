0da6ee19a18dd7a4b132e5df2e80f1d17f227d88
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-02-20 13:01:09 -0300

    5 day swift

--- 100-days-swift.playground/Contents.swift
+++ 100-days-swift.playground/Contents.swift
@@ -1,7 +1,61 @@
-//This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
+let aux1 = "YAnderson"
+let aux2 = "Bikers"
 
-let arrayString = [String](["enzo", "perez", "enzo"])
-let a = arrayString.count
-let s = Set(arrayString)
-print(a)
-print(s.count)
+//with strings  sign >< and = compare the alphabetical order
+if(aux1 < aux2){
+    print("first \(aux1) and \(aux2)")
+} else {
+    print("first \(aux2) and \(aux1)")
+}
+
+
+//isEmpty is more performant that =="" or .count == 0
+let c = "a"
+let d = "aux"
+
+if(d.isEmpty ){
+    print("VACIO")
+} else {
+    print("CONTIENE LETRAS")
+}
+
+//comparition with enums
+enum TransportOption {
+    case airplane, bus, car, helicopter, biclycle, scooter
+}
+
+enum Movies {
+    case batman, superman, marvel, dc, spiderman
+}
+
+let transport = TransportOption.helicopter
+
+if(transport == .bus || transport == .car){ //only dots because Swift knows transport must be some kind of TransportOption
+    print("Trafic!")
+} else if(transport == .airplane || transport == .helicopter){
+    print("Flying!")
+} else {
+    print("Walking")
+}
+
+
+//switch fallthrough
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+case 2:
+    print("2 turtle doves")
+default:
+    print("A partridge in a pear tree")
+}
+
+
