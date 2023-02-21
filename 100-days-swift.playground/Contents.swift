725db27314ec4c61935f49dc604142bdeb7d897a
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-02-21 10:34:09 -0300

    7 day swift - functions

--- 100-days-swift.playground/Contents.swift
+++ 100-days-swift.playground/Contents.swift
@@ -1,35 +1,47 @@
-//var lyric = "Haters gonna"
-//
-//for i in lyric {
-//    print("my: ", i)
-//}
-//
-//
-//print("Tu", 100 % 1000)
+func printMyMsg(msg: String, number: Int) -> Void {
+    print(msg, number)
+}
+
+printMyMsg(msg: "Hola", number: 9)
+//printMyMsg(number: 8, msg: "Error")
+
+func rollDice() -> Int {
+    Int.random(in: 1...6)
+}
+
+print(rollDice())
+
+func areEqualString(str1: String, str2: String) -> Bool {
+    return str1.sorted() == str2.sorted()
+}
+
+areEqualString(str1: "abc", str2: "cab")
+
+func helpTuple(name: String, age: Int) -> (name: String, age: Int) {
+    return (name,age)
+}
+
+let (name, age) =  helpTuple(name: "enzou", age: 18)
+
+print("\(name) has \(age)")
 
 
-//continues
-//let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
-//
-//for filename in filenames {
-//    if filename.hasSuffix(".jpg") == false {
-//        break
-//    }
-//
-//    print("Found picture: \(filename)")
-//}
+//func names parameters
+//EXTERNAL - INTERNAL!!
+func helpUpper(mystring passedString: String) -> Bool {
+    print(passedString)
+    return passedString == "hola"
+}
+
+func helpUpper(_ passedString: String) -> Bool {
+    print(passedString)
+    return passedString == "hola"
+}
+
+helpUpper("hola sin names")
+helpUpper(mystring: "hola")
 
 
-//Checkpont: fizzbuzz problem
-
-for n in 1...100 {
-    if n % 3 == 0 && n % 5 == 0 {
-        print("fizz buzz")
-    } else if(n % 3 == 0) {
-        print("Fizz")
-    } else if(n % 5 == 0) {
-        print("buzz")
-    } else {
-        print("number: ", n)
-    }
+func evaluateJavaScript(_ input: String) {
+    print("Yup, that's JavaScript alright.")
 }
