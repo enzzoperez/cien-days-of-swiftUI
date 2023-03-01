0c03ad22949e5f888126e1cb6738ad108cc9bcf3
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-03-01 15:52:34 -0300

    10 days swift struct part 1

--- 100-days-swift.playground/Contents.swift
+++ 100-days-swift.playground/Contents.swift
@@ -1,79 +1,35 @@
-let myPrint = {
-    print("This is a basic closure")
-}
-myPrint()
+//structs functions
 
-
-//closure with errors!
-enum myError: Error { case MyError }
-let myClosureParams = {(number: Int, name: String) throws -> String in
-    if(name == "enzou"){
-        throw myError.MyError
+struct MyHome {
+    var dir: String
+    var distance: Int = 0 {
+        willSet{
+            //not work: distance = 42
+            print("antes: \(distance), va a cambiar a", newValue)
+        }
+        didSet{
+            distance = 42
+            print("deszpues: \(distance), venia de", oldValue)
+            
+        }
+        
     }
-    return "my \(name) is \(number)"
 }
 
-do{
-    let a = try myClosureParams(4, "enzou")
-    print(a)
-} catch myError.MyError {
-    print("error in screen")
-}
+var a = MyHome(dir: "CASEROs", distance: 30)
+a.distance += 2
 
-//with func
-func myFuncCl(name: String, num: String) -> Int {
-     42
-}
 
-var b = myFuncCl
-b("E", "ERT")
+//inits
+struct Player {
+    let name: String
+    let number: Int
 
-//passing func
-func makeArray(size: Int, using generator: () -> Int) -> [Int] {
-    var numbers = [Int]()
-
-    for _ in 0..<size {
-        let newNumber = generator()
-        numbers.append(newNumber)
+    init(name: String) {
+        self.name = name
+        number = 32
     }
-
-    return numbers
-}
-let rolls = makeArray(size: 50, using: {
-    Int.random(in: 1...3)
-})
-//let rolls = makeArray(size: 50) {
-//    Int.random(in: 1...20)
-//}
-
-
-//print(rolls)
-
-
-//CHECKPOINT
-
-//My solution (too JS based)
-print("Checkpoint started")
-let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
-
-func myEven(n: Int) -> Bool { n % 2 != 0 }
-func sortArray(item1: Int, item2: Int) -> Bool {item1 < item2}
-func convert(item: Int) -> Void { print("\(item) is a lucky numbers") }
-
-luckyNumbers
-    .filter(myEven)
-    .sorted(by: sortArray)
-    .map(convert)
-
-
-//proposed solution
-let luckyNumbersA = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
-
-let myFilter = { (array: [Int]) in
-    array
-        .filter{ !$0.isMultiple(of: 2)}
-        .sorted()
-        .map{print("\($0) is a lucky number")}
 }
 
-myFilter(luckyNumbersA)
+let player = Player(name: "Megan R")
+print(player.number)
