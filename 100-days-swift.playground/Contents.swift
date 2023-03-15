ed1238ac34881acd75a4ed20b5cb82be175e7ae6
enzou <enzou@MacBook-Pro-de-enzou.local>
2023-03-14 23:38:35 -0300

    11 days swift -  structs 2 and checkpoint

--- 100-days-swift.playground/Contents.swift
+++ 100-days-swift.playground/Contents.swift
@@ -1,35 +1,47 @@
-//structs functions
+//If you want to mix and match static and non-static properties and methods, there are two rules:
+//
+//To access non-static code from static code… you’re out of luck: static properties and methods can’t refer to non-static properties and methods because it just doesn’t make sense – which instance of School would you be referring to?
+//To access static code from non-static code, always use your type’s name such as School.studentCount. You can also use Self to refer to the current type.
+struct School {
+    var studentCount = 0
+    static var name = "ENZOU"
 
-struct MyHome {
-    var dir: String
-    var distance: Int = 0 {
-        willSet{
-            //not work: distance = 42
-            print("antes: \(distance), va a cambiar a", newValue)
-        }
-        didSet{
-            distance = 42
-            print("deszpues: \(distance), venia de", oldValue)
-            
-        }
-        
+    static func add(student: String) {
+        print("\(student) joined the school.")
+//        studentCount += 1
+    }
+    
+    func printName() {
+        print("my name is \(Self.name)", School.name)
     }
 }
 
-var a = MyHome(dir: "CASEROs", distance: 30)
-a.distance += 2
+School.add(student: "init")
 
+//checkpoint structs: create car structs and methos to change gear
 
-//inits
-struct Player {
-    let name: String
-    let number: Int
-
-    init(name: String) {
-        self.name = name
-        number = 32
+struct Car {
+    static let model: String = "MODEL CAR"
+    let numberOfSeats: Int = 9
+    var currentGear: Int = 0
+    
+    mutating func changeGear(to: String) {
+        if to.uppercased() == "UP" && currentGear < 5 {
+            currentGear += 1
+        }
+        if to.uppercased() == "DOWN" && currentGear > 0 {
+            currentGear -= 1
+        }
     }
 }
 
-let player = Player(name: "Megan R")
-print(player.number)
+var car = Car()
+car.changeGear(to: "UP")
+car.changeGear(to: "UP")
+car.changeGear(to: "UP")
+car.changeGear(to: "UP")
+car.changeGear(to: "UP")
+car.changeGear(to: "UP")
+print(car.currentGear)
+
+//could we find some workaround with observers?
